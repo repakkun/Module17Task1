1ed7ca2672eb6a1c755073a5c72bd5356f40a3a8
rePakkun <cj.ilya@yandex.ru>
2021-01-21 22:14:42 +0300

    Добавьте файлы проекта.

---
+++ Module17Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module17Task1
+{
+    //Своё исключение
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+    class Program
+    {
+        static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[5] { new ArgumentException(), new DivideByZeroException(), new FieldAccessException(), new FormatException(), new MyException("мое исключение") };
+            try
+            {   
+                int result = Division(1, 0);                          
+                throw new ArgumentException();
+                throw new DivideByZeroException();               
+                throw new FieldAccessException();
+                throw new FormatException(); 
+            }
+            catch(Exception)
+            {
+                /* for (int i = 0; i < exceptions.Length; i++) 
+                 {                  
+                     {
+                         Console.WriteLine(ex.Message);
+                     }
+                 }*/
+                foreach (Exception ex in exceptions)
+                {                 
+                    Console.WriteLine(ex.Message);
+                }                    
+            }
+            Console.ReadKey();
+        }
+    }
+}
